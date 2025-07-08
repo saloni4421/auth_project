534cd17e318dbd3e9bbbd615bdb355b20ea319a0
Saloni Patel <104291997+saloni4421@users.noreply.github.com>
2025-07-08 10:07:51 +0530

    Delete auth_project directory

--- auth_project/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('signup/', views.signup, name='signup'),
-    path('profile/', views.profile, name='profile'),
-]
